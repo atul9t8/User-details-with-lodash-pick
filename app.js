d3673ee9f75f82fbcc8f1e0b42c5fd7fbeab7cfd
atul9t8 <atuldebnath8@yahoo.com>
2022-09-08 23:13:36 +0600

    cors issue solved

--- app.js
+++ app.js
@@ -9,12 +9,7 @@ const userRoute = require("./routers/userDetailsRouter")
 require('dotenv').config()
 mongoose.connect(process.env.DB)
 
-app.use(
-    cors({
-        origin: "http://localhost:3001",
-        credentials: true
-    })
-)
+app.use(cors())
 
 app.use('/user', userRoute)
 
