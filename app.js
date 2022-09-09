345138dfcee86bcbf4f6093bf7c8aa303e3c4b1e
atul9t8 <atuldebnath8@yahoo.com>
2022-09-09 13:03:45 +0600

    done

--- app.js
+++ app.js
@@ -17,6 +17,17 @@ app.use(
 
     })
 )
+app.use((req, res, next) => {
+    //allow access from every, elminate CORS
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.removeHeader('x-powered-by');
+    //set the allowed HTTP methods to be requested
+    res.setHeader('Access-Control-Allow-Methods', req.method);
+    //headers clients can use in their requests
+    res.setHeader('Access-Control-Allow-Headers','Content-Type');
+    //allow request to continue and be handled by routes
+    next();
+  });
 
 app.use('/user', userRoute)
 
