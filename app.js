4fa7730b3d869cdecdebf02f5bfd8f9a1560242c
atul9t8 <atuldebnath8@yahoo.com>
2022-09-09 14:02:50 +0600

    All Done

--- app.js
+++ app.js
@@ -10,24 +10,9 @@ mongoose.connect(process.env.DB)
 
 app.use(
     cors({
-        "origin": "*",
-        // "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-        // "preflightContinue": false,
-        // "optionsSuccessStatus": 204
-
+        "origin": "*"
     })
 )
-// app.use((req, res, next) => {
-//     //allow access from every, elminate CORS
-//     res.setHeader('Access-Control-Allow-Origin','*');
-//     res.removeHeader('x-powered-by');
-//     //set the allowed HTTP methods to be requested
-//     res.setHeader('Access-Control-Allow-Methods', req.method);
-//     //headers clients can use in their requests
-//     res.setHeader('Access-Control-Allow-Headers','Content-Type');
-//     //allow request to continue and be handled by routes
-//     next();
-//   });
 
 app.use('/user', userRoute)
 
