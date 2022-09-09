6daefc75a0c10bf3ed9ecd49f5e4440975918ff9
atul9t8 <atuldebnath8@yahoo.com>
2022-09-09 12:51:19 +0600

    done

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const cors = require('cors');
 const express = require("express");
+const cors = require('cors');
 const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
